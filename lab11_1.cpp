e6e65ef5a685704abefe5a04ea47825a95003d41
naechutimon <chutimonnaenae@gmail.com>
2024-12-24 14:30:21 +0700

    chunelab11/1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,26 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main() 
+{
+    srand(time(0));
+    string grades[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future." << "\n";
+    
+    int i =0;
+    while(i<3)
+    {
+        cin.ignore();
+        i++;
+    }
+    
+    string prediction = grades[rand()%9];
+    cout <<"You will get " << prediction <<" in this 261102."<<"\n";
+    
+    return 0;
+}
